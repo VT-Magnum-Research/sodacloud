e532ce46f3c08de0d81f68d5f4607c856f1a8b31
Hamilton Turner <hamiltont@gmail.com>
2013-02-27 21:38:56 -0500

    Making logstash work

--- SodaCloudJetty/logs/run_logstash.sh
+++ SodaCloudJetty/logs/run_logstash.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-echo "Running logstash using the default options"
+if [ -z "$1" ]
+then
+	echo "Usage: $0 <logstash.conf>"
+	exit
+fi
 
-java -jar logstash-1.1.9-monolithic.jar agent -f logstash.conf
+echo "Using configuration at $1"
+echo "   If errors occur, check that $1 points to the absolute path of the log files on *your* filesystem"
+echo "Starting logstash monolithic...this could take a while" 
+java -jar logstash-1.1.10.dev-monolithic.jar agent -f $1 -- web --backend elasticsearch:///?local
+#java -jar logstash-1.1.9-monolithic.jar agent -f $1
