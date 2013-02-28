ce9f9c99cbbcd1f22c320dd009eb81cdf38038d1
hamiltont <hamiltont@ataack.ece.vt.edu>
2013-02-27 22:34:15 -0500

    Logstash working

--- SodaCloudJetty/logs/run_logstash.sh
+++ SodaCloudJetty/logs/run_logstash.sh
@@ -7,7 +7,9 @@ then
 fi
 
 echo "Using configuration at $1"
-echo "   If errors occur, check that $1 points to the absolute path of the log files on *your* filesystem"
+echo "   If errors occur, check that $1 points to the absolute"
+echo "   path of the log files on *your* filesystem"
 echo "Starting logstash monolithic...this could take a while" 
-java -jar logstash-1.1.10.dev-monolithic.jar agent -f $1 -- web --backend elasticsearch:///?local
-#java -jar logstash-1.1.9-monolithic.jar agent -f $1
+echo "   Wait until you see 'remaining' printed out to try"
+echo "   accessing localhost:9292"
+java -jar logstash-1.1.9-monolithic.jar agent -f $1 -- web --backend elasticsearch:///?local
