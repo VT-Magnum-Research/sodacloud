451d521845c0b187f118a02c9f2c80e24009950d
Akshay Giridhar <akshay87@vt.edu>
2013-05-21 23:49:39 -0400

    Network Issue

--- SodaCloudJetty/src/org/magnum/soda/server/wamp/adapters/jetty/JettyServer.java
+++ SodaCloudJetty/src/org/magnum/soda/server/wamp/adapters/jetty/JettyServer.java
@@ -16,25 +16,68 @@
 package org.magnum.soda.server.wamp.adapters.jetty;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
+import org.eclipse.jetty.websocket.WebSocket;
 import org.eclipse.jetty.websocket.WebSocket.OnTextMessage;
+import org.eclipse.jetty.websocket.WebSocketConnectionRFC6455;
 import org.magnum.soda.server.wamp.ClientId;
 import org.magnum.soda.server.wamp.WampServer;
 
 
 public class JettyServer implements OnTextMessage {
+	
+	WebSocket.FrameConnection _conn;
+	
 	public JettyServer(WampServer wampServer) {
 		this.wampServer = wampServer; 
 	}
 
 	@Override
 	public void onOpen(Connection connection) {
+
+		System.out.println("JettyServer: "+"Open Connection");
+		if(connection instanceof WebSocket.FrameConnection)
+		{
+		this._conn=(WebSocket.FrameConnection)connection;
+		}
+		/*
+		 * This is a temporary code to increase the timeout for a single endpoint created by 
+		 * Jetty server.
+		 * TO change the timeout input a number in ms in the below shown
+		 * setMaxIdleTime(xx);
+		 */
+		try {
+			 Field this$0 = _conn.getClass().getDeclaredField("this$0");
+			 this$0.setAccessible(true);
+			 WebSocketConnectionRFC6455 w=(WebSocketConnectionRFC6455)this$0.get(_conn);
+			 System.out.println("JettyServer: "+"Current Timeout"+w.getEndPoint().getMaxIdleTime());
+			 w.getEndPoint().setMaxIdleTime(3600000);
+			 System.out.println("JettyServer: "+"Timeout Set"+w.getEndPoint().getMaxIdleTime());
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		clientId = wampServer.addClient(new ChannelToConnectionAdapter(connection));
 	}
 
 	@Override
 	public void onClose(int closeCode, String message) {
-	}
+		System.out.println("close connection");
+		}
 
 	public void onMessage(String data) {
 		try {
