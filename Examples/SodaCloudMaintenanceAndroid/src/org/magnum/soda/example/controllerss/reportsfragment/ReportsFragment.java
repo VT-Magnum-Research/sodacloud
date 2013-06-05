3367c2887c20b6c821bdbf90ae584d1eef0a5cce
hickeyadamc <hickeyadamc@gmail.com>
2013-06-05 16:25:23 -0400

    Minor changes to create report UI, reports fragment

--- Examples/SodaCloudMaintenanceAndroid/src/org/magnum/soda/example/controllerss/reportsfragment/ReportsFragment.java
+++ Examples/SodaCloudMaintenanceAndroid/src/org/magnum/soda/example/controllerss/reportsfragment/ReportsFragment.java
@@ -1,7 +1,20 @@
 package org.magnum.soda.example.controllerss.reportsfragment;
 
+import org.magnum.soda.example.maint.R;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.actionbarsherlock.app.SherlockFragment;
 
 public class ReportsFragment extends SherlockFragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_reports, container,false);
+	}
 	
 }
